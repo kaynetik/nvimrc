b17b966f861f191ee8c3d27321aa7a2565223c65
kaynetik <aleksandar@nesovic.dev>
2024-04-12 19:03:52 +0200

    feat: Enable workspaces telescope picker

--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -1,21 +1,20 @@
-return {
-  -- TODO: Play a bit with this one to confirm it's working as expected!
-  {
-    'natecraddock/workspaces.nvim',
-    config = function()
-      require('workspaces').setup {
-        autodetect = true,
-        hooks = {
-          open = function()
-            local wsp = require('workspaces').path()
-            print('Setting workspace path to: ' .. wsp)
-            vim.g.WORKSPACE_ROOT_PATH = wsp
-
-            require('telescope.builtin').find_files()
-          end,
-          -- open = 'Telescope find_files',
-        },
-      }
-    end,
-  },
+local M = {
+  'natecraddock/workspaces.nvim',
 }
+
+M.config = function()
+  require('workspaces').setup {
+    autodetect = true,
+    hooks = {
+      open = function()
+        local wsp = require('workspaces').path()
+        vim.notify('Setting WorksSpace path to: ' .. wsp, vim.log.levels.INFO)
+        vim.g.WORKSPACE_ROOT_PATH = wsp
+
+        require('telescope.builtin').find_files()
+      end,
+    },
+  }
+end
+
+return M
