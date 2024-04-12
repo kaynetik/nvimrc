1a349753cd4b1e57e7f8ef9d22d738ab711a5f2e
kaynetik <aleksandar@nesovic.dev>
2024-04-12 18:43:37 +0200

    feat: Enable `copilot` and fix `neotree`

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+local M = {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  build = ':Copilot auth',
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+    },
+    panel = { enabled = false },
+  },
+}
+
+return M
